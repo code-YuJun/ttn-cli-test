d1887332c4299cb8ea49e888ef467081616d1579
code-YuJun <2594131385@qq.com>
2026-06-16 13:07:13 +0800

    feat: 项目拉取

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    assetsDir: '',
+    manifest: 'manifest.json',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const extType = assetInfo.name.split('.').pop()
+          if (/png|jpe?g|gif|svg|webp|ico/.test(extType)) {
+            return 'img/[name].[hash][extname]'
+          }
+          if (/css/.test(extType)) {
+            return 'css/[name].[hash][extname]'
+          }
+          return 'assets/[name].[hash][extname]'
+        },
+        chunkFileNames: 'js/[name].[hash].js',
+        entryFileNames: 'js/[name].[hash].js',
+      },
+    },
+  },
 })
